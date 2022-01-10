4ff00a8e78ec243d2991b810c63af8e952003c88
Atlas <amouri2000@hotmail.com>
2022-01-10 16:47:59 -0600

    Created Main Application Method

--- src/main/java/com/bugbuddy/App.java
+++ src/main/java/com/bugbuddy/App.java
@@ -1,13 +1,28 @@
 package com.bugbuddy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            // Initializing
+            MongoClient dbClient = MongoClients.create("PLEASE REPLACE WITH DB CONNECTION STRING");
+            StandardIO io = new StandardIO();
+            MongoCollection<Document> bugsCollection = dbClient.getDatabase("bugs").getCollection("main");
+            CommandHandler handler = new CommandHandler(bugsCollection, io);
+
+            // Main program
+            io.update("Chose one of the following options:\n1 - Create bug report\n2 - Update bug report by ID\n"
+                    + "3 - Mark bug report solved by ID\n4 - Mark bug report as unsolved by ID\n5 - Print bug report by ID"
+                    + "\n6 - Delete bug report by ID\n0 - Exit\n");
+            while (true) {
+                handler.handleCommand(io.getInput());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
